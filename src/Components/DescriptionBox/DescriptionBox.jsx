ec2c986c5016fb7a23251863eba818dce0f54a64
Mayank Chauhan <mayankchauhan1974@gmail.com>
2024-09-06 07:40:28 +0530

    latest project

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description"><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci at quia quisquam totam magni corrupti dolorum inventore, maiores id deserunt molestiae doloribus harum esse cumque sapiente quos mollitia rerum sint aspernatur ipsam autem impedit hic quae. Suscipit fugit ipsum dolorum, ducimus sapiente repellendus odit reprehenderit nisi. Beatae deserunt tempore error?</p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur molestiae quaerat obcaecati cupiditate amet sed.
+        </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
